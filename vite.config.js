46e6e0cff6d89e8d998add450faf2be697ea0caf
satyamrathod45 <satyamrathodi70@gmail.com>
2025-08-10 16:03:47 +0530

    this is last one

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const isGitHubPages = process.env.GITHUB_REPOSITORY?.includes('satyamrathod45/onepiece');
-
 export default defineConfig({
-  base: isGitHubPages ? '/onepiece/' : '/',
+  base: '/onepiece/',
   plugins: [react()],
 });
